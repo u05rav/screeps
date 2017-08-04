b32dec5eb9af5fc1c8ea6e47296ac707c1497b61
Your Name <rveitch@maxeler.com>
2017-08-04 12:07:57 -0700

    add state machine

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,5 +1,15 @@
 module.exports = {
     run: function(creep){
+
+        if(creep.memory.working == true && creep.carry.energy == 0){
+            creep.memory.working = false;
+        }
+
+        if(creep.memory.working == false && creep.carty.energy == creep.carryCapacity)
+            creep.memory.working = true;
+        }
+            
+
         if(creep.memory.working){
             console.log("working");
         }else{
