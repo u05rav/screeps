490866c0b7604e3f5566144dccd6f249dbacb0ab
Your Name <rveitch@maxeler.com>
2017-08-04 12:34:34 -0700

    add builders

---
+++ role.builder.js
@@ -0,0 +1,33 @@
+var roleUpgrader = require('role.upgrader');
+
+module.exports = {
+    run: function(creep){
+
+        if(creep.memory.working == true && creep.carry.energy == 0){
+            creep.memory.working = false;
+        }
+
+        if(creep.memory.working == false && creep.carry.energy == creep.carryCapacity){
+            creep.memory.working = true;
+        }
+            
+
+        if(creep.memory.working){
+            var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+
+            if(constructionSite != undefined){
+                if(creep.build(constructionSite)==ERR_NOT_IN_RANGE){
+                    creep.moveTo(constructionSite);
+                }
+            }else{
+                roleUpgrader.run(creep);
+            }
+
+        }else{
+            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE){
+                creep.moveTo(source);
+            }
+        }
+    }
+}
